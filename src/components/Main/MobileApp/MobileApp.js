41d0d9eacb7b9965086d3a5baba9010a72ec6e2b
Viktor Ivanov <sessgreem@gmail.com>
2021-03-02 13:19:59 +0200

    MobileApp section styled

--- src/components/Main/MobileApp/MobileApp.js
+++ src/components/Main/MobileApp/MobileApp.js
@@ -1,15 +1,15 @@
 import classes from "./MobileApp.module.css";
 import React from "react";
 import mobilePhone from "../../../assets/images/mobilephone.webp";
+import googlePlay from "../../../assets/Google_Play_Store_badge_EN.svg";
+import appStore from "../../../assets/Download_on_the_App_Store_Badge.svg";
+
 const appStoreUrl = "#";
 const playStoreUrl = "#";
 
 const MobileApp = () => {
     return (
         <section className={classes.MobileApp}>
-            <div className={classes.MobileImage}>
-                <img src={mobilePhone} alt="Mobile Phone" />
-            </div>
             <div className={classes.MobileOffer}>
                 <h2>Save $$$</h2>
                 <h2>
@@ -17,8 +17,12 @@ const MobileApp = () => {
                     demand
                 </h2>
                 <div className={classes.MobileAppDownload}>
-                    <a href={appStoreUrl}>Downloadappstore</a>
-                    <a href={playStoreUrl}>downloadandroid</a>
+                    <a href={appStoreUrl}>
+                        <img src={googlePlay} alt="Google Play" />
+                    </a>
+                    <a href={playStoreUrl}>
+                        <img src={appStore} alt="App Store" />
+                    </a>
                 </div>
                 <p className={classes.MobileLegal}>
                     Apple and the Apple logo are trademarks of Apple Inc.,
@@ -26,6 +30,9 @@ const MobileApp = () => {
                     service mark of Apple Inc. Google Play is a trademark of
                     Google Inc. Terms apply.
                 </p>
+            </div>
+            <div className={classes.MobileImage}>
+                <img src={mobilePhone} alt="Mobile Phone" />
             </div>
         </section>
     );
