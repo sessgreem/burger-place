ee1c27c0d07d7c8837496bedcadeccde38aa4f4c
Viktor Ivanov <sessgreem@gmail.com>
2021-03-04 21:47:27 +0200

    Added more items into the menu and made it responsive

--- src/components/Main/Menu/Menu.js
+++ src/components/Main/Menu/Menu.js
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 import classes from "./Menu.module.css";
 import Card from "./Card/Card";
 
-// import burger from "../../../assets/images/burgers/whopper-cheeseburger.png";
-import ourFavorites from "../../../assets/images/our_favorites_section.webp";
+import ourFavorites from "../../../assets/images/menu/our_favorites_section.webp";
+
+import sweets from "../../../assets/images/menu/sweets.webp";
+import familyBundle from "../../../assets/images/menu/familybundle.webp";
+import chickenFish from "../../../assets/images/menu/chiken.webp";
+import queenJr from "../../../assets/images/menu/f4dc27eb7337f7cbd12d3ccd840e6a031fcba622-360x270.webp";
+import drinksCoffe from "../../../assets/images/menu/drinksandcoffe.webp";
 
 const Menu = () => {
     const [cardInfo] = useState([
         {
-            name: "Our Favorites",
-            description: "A selection of our favorite items for you.",
-            img: ourFavorites,
+            name: "Family Burgers",
+            description: "The best food bundles for every family!",
+            img: familyBundle,
         },
         {
             name: "Our Favorites",
@@ -18,9 +23,24 @@ const Menu = () => {
             img: ourFavorites,
         },
         {
-            name: "Our Favorites",
-            description: "A selection of our favorite items for you.",
-            img: ourFavorites,
+            name: "Chicken & Fish",
+            description: "Do you prefer Chicken? We have that too. ",
+            img: chickenFish,
+        },
+        {
+            name: "Queen Jr",
+            description: "The Queen is waiting for you!",
+            img: queenJr,
+        },
+        {
+            name: "Drinks & Coffee",
+            description: "Get you favorite drinks from us!",
+            img: drinksCoffe,
+        },
+        {
+            name: "Sweets",
+            description: "Feeling something sweet? You want to see theese!",
+            img: sweets,
         },
     ]);
 
