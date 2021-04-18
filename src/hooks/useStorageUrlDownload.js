1d69afaf676b2cd47f8ca8075d105f7f064bd1e4
Viktor Ivanov <sessgreem@gmail.com>
2021-04-18 15:13:26 +0300

    fixed useState memory leak caused by useStorageUrlDownload Hook

--- src/hooks/useStorageUrlDownload.js
+++ src/hooks/useStorageUrlDownload.js
@@ -1,18 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { storage } from "../firebase/index";
 
 const useStorageUrlDownload = (imgRefURL) => {
     const [imageURL, setImageURL] = useState(null);
+    const imgRef = storage.refFromURL(imgRefURL);
+
+    const fetchImageUrl = useCallback(
+        async (controller) => {
+            try {
+                const signal = controller.signal;
+                const url = await imgRef.getDownloadURL();
+                if (!signal.aborted) {
+                    setImageURL(url);
+                }
+            } catch (error) {
+                if (error.name === "AbortError")
+                    console.log("Request Aborted!");
+                else console.error(error);
+            }
+        },
+        [imgRef]
+    );
 
     useEffect(() => {
-        const imgRef = storage.refFromURL(imgRefURL);
-        imgRef
-            .getDownloadURL()
-            .then((URL) => {
-                setImageURL(URL);
-            })
-            .catch((error) => console.log(error));
-    }, [imgRefURL]);
+        const controller = new AbortController();
+        fetchImageUrl(controller);
+        return () => controller.abort();
+    }, [fetchImageUrl]);
 
     return imageURL;
 };
